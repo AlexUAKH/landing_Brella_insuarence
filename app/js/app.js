76c03c9a0cf4ee9138d2daec93d4cdbbf0469992
AlexUAKH <sashaos1404@gmail.com>
2021-11-02 16:35:24 +0200

    header done

--- app/js/app.js
+++ app/js/app.js
@@ -3,4 +3,32 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   // Custom JS
+  const menus = document.querySelectorAll(".menu");
+  const arrows = document.querySelectorAll(".arrow");
+  const burger = document.getElementById("burger");
+  const navMenu = document.getElementById("mainMenu");
+  const body = document.querySelector("body");
+
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    body.classList.toggle("locked");
+  });
+
+  if (arrows) {
+    for (const arrow of arrows) {
+      arrow.previousElementSibling.classList.add("parent");
+    }
+  }
+
+  if (menus) {
+    for (const menu of menus) {
+      console.log("m", menu);
+
+      menu.addEventListener("click", e => {
+        e.preventDefault();
+        menu.classList.toggle("active");
+      });
+    }
+  }
 });
